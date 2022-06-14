565c070d7d3e8d5d6553901f11ff82f3065c2986
pejath <pejath@yandex.ru>
2022-06-14 14:40:05 +0300

    Working hash_id searcher

--- app/graphql/ql_schema.rb
+++ app/graphql/ql_schema.rb
@@ -29,17 +29,12 @@ class QlSchema < GraphQL::Schema
   end
 
   # Union and Interface Resolution
-  def self.resolve_type(abstract_type, obj, ctx)
-    # to return the correct GraphQL object type for `obj`
-    if abstract_type.kind.object?
-      abstract_type
-    else
-      raise(GraphQL::RequiredImplementationMissingError)
-    end
+  def self.resolve_type(_abstract_type, obj, ctx)
+    "Types::#{obj.class.base_class.name}".constantize
   end
 
-  def self.id_from_object(object,_type_definition, query_ctx)#, q = nil)
-    generate_global_id(object.class.name.to_s, object.id, query_ctx)
+  def self.id_from_object(object,_type_definition, query_ctx)
+    generate_global_id(object.class.name.to_s, object.id)
   end
 
   # Given a string global id, find the object
@@ -49,37 +44,33 @@ class QlSchema < GraphQL::Schema
     urn = if id.is_a?(::URN::Generic)
             id
           else
-            parse_global_id(id, query_ctx)
+            parse_global_id(id)
           end
 
     urn.entity_class.find_by(id: urn.entity_id)
   end
 
-  def self.generate_global_id(entity_type, entity_id, query_ctx)
+  def self.generate_global_id(entity_type, entity_id)
     scope = IdHasher.make_scope(entity_type)
-    # hashed_id = id_hasher(query_ctx.fetch(:hashing_salt)).encode(type: scope, id: entity_id)
     hashed_id = IdHasher.new.encode(type: scope, id: entity_id)
 
     URN::Mazepay.new_hashed_id(scope, hashed_id).to_urn
   end
 
   # Given a global ID (URN), decode the ID and create a new URN with the clear ID.
-  def self.parse_global_id(id, query_ctx)
+  def self.parse_global_id(id)
     urn = ::URN.parse(id)
 
     # For now, always reject un-hashed IDs. This may become an environment or schema setting.
     # May be desirable to return this as an error or just `nil`?
-    # raise GraphQL::ExecutionError, "Entity ID not accepted." unless urn.hashed_id?
-    # raise GraphQL::ExecutionError, "Entity ID not accepted." unless urn.hashed_id?
     clear_ids = IdHasher.new.decode(scope: urn.entity_type, hashed_id: urn.hashed_id)
-    # clear_ids = IdHasher.new.decode(scope: urn.nid, hashed_id: urn.nss)
     raise GraphQL::ExecutionError, "Invalid hash" if clear_ids.none?
 
     clear_id = clear_ids.fetch(0)
 
     URN::Mazepay.new_clear_id(urn.entity_type, clear_id)
-  # rescue URN::ParseError => ex
-  #   raise GraphQL::ExecutionError, ex.message
+  rescue URN::ParseError => ex
+    raise GraphQL::ExecutionError, ex.message
   end
 
   private
