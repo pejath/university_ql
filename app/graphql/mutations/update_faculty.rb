68e4a90b7c590f24e0970551535ffa7bc6fcd69b
pejath <pejath@yandex.ru>
2022-06-21 11:27:14 +0300

    Faculty mutations and mutation tests updated to global_id

--- app/graphql/mutations/update_faculty.rb
+++ app/graphql/mutations/update_faculty.rb
@@ -1,13 +1,12 @@
 module Mutations
   class UpdateFaculty < Mutations::BaseMutation
-    argument :id, ID, required: true
+    argument :faculty_id, Types::GlobalId, required: true, loads: Types::Faculty
     argument :name, String, required: false
     argument :formation_date, GraphQL::Types::ISO8601Date, required: false
 
     type Types::Faculty
 
-    def resolve(id:, name:, formation_date:)
-      faculty = Faculty.find(id)
+    def resolve(faculty:, name:, formation_date:)
       faculty.update(
         name: name,
         formation_date: formation_date
