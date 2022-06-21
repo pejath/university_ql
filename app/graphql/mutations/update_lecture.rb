42bf2f27880ed8a940c150d7f9ff59bf43fbb4ca
pejath <pejath@yandex.ru>
2022-06-21 11:26:14 +0300

    Lecture mutations and mutation tests updated to global_id

--- app/graphql/mutations/update_lecture.rb
+++ app/graphql/mutations/update_lecture.rb
@@ -1,10 +1,10 @@
 module Mutations
   class UpdateLecture < BaseMutation
-    argument :id, ID, required: true
-    argument :group_id, ID, required: true
-    argument :subject_id, ID, required: true
-    argument :lecturer_id, ID, required: true
-    argument :lecture_time_id, ID, required: true
+    argument :lecture_id, Types::GlobalId, required: true, loads: Types::Lecture
+    argument :group_id, Types::GlobalId, required: true, loads: Types::Group
+    argument :subject_id, Types::GlobalId, required: true, loads: Types::Subject
+    argument :lecturer_id, Types::GlobalId, required: true, loads: Types::Lecturer
+    argument :lecture_time_id, Types::GlobalId, required: true, loads: Types::LectureTime
 
     argument :corpus, Integer, required: true
     argument :weekday, Types::Weekday, required: true
@@ -12,13 +12,12 @@ module Mutations
 
     type Types::Lecture
 
-    def resolve(id:, group_id:, subject_id:, lecturer_id:, lecture_time_id:, corpus:, weekday:, auditorium:)
-      lecture = Lecture.find(id)
+    def resolve(lecture:, group:, subject:, lecturer:, lecture_time:, corpus:, weekday:, auditorium:)
       lecture.update(
-        group_id: group_id,
-        subject_id: subject_id,
-        lecturer_id: lecturer_id,
-        lecture_time_id: lecture_time_id,
+        group: group,
+        subject: subject,
+        lecturer: lecturer,
+        lecture_time: lecture_time,
         corpus: corpus,
         weekday: weekday,
         auditorium: auditorium
